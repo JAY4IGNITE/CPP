d6872231f9a8466bd795cc71008aef08044d7a24
JAYA SAI KRISHNA <vjayasai3@gmail.com>
2025-07-18 10:08:39 +0530

    queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -5,10 +5,7 @@ int main(){
     int front = -1,rear = -1;
     enqueue(q,front,rear,10);
     enqueue(q,front,rear,20);
-    enqueue(q,front,rear,30);
-    enqueue(q,front,rear,40);
     dequeue(q,front,rear);
-    enqueue(q,front,rear,50);
-    enqueue(q,front,rear,60);
+    enqueue(q,front,rear,30);
     display(q,front,rear);
 }
